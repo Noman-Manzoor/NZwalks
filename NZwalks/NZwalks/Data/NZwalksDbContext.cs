e478a7bd9c878c241f8922902f0ada3b886b8547
Noman <maliknomi1032@gmail.com>
2023-01-10 09:47:38 -0500

    DB setup done

---
+++ NZwalks/NZwalks/Data/NZwalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.Models.Domain;
+
+namespace NZwalks.Data
+{
+    public class NZwalksDbContext: DbContext
+    {
+        public NZwalksDbContext(DbContextOptions<NZwalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region>    Regions { get; set; }
+        public DbSet<Walk>    Walks { get; set; }
+        public DbSet<WalkDifficulty>    walkDifficulties { get; set; }
+    }
+}
